8d07c82080bdef89b897f1d030d708c7d9245715
Amine Bourouba <amine@evercam.io>
2022-08-06 20:27:22 +0100

    Feat: Add ContactCard and LandingPage components

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-card v-if="contact">
+      <v-img
+        :src="contact.src"
+        class="white--text align-end"
+        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+        height="250px"
+      />
+
+      <v-card-actions>
+        <v-icon class="mr-1">mdi-email</v-icon>
+        <span class="truncate">{{ contact.name }}</span>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ContactCard",
+  props: {
+    contact: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+});
+</script>
+<style scoped>
+.truncate {
+  width: 250px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,13 +1,60 @@
 <template>
-  <v-container> </v-container>
+  <div>
+    <v-row class="pa-4">
+      <v-col
+        v-for="(contact, index) in contacts.data"
+        :key="index"
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-skeleton-loader
+          v-if="contacts.isFetching"
+          class="mx-auto"
+          max-width="300"
+          type="card"
+        ></v-skeleton-loader>
+        <ContactCard v-else :contact="contact" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
+<script setup lang="ts">
+import { computed, reactive, ref } from "vue";
+import { useFetch } from "@vueuse/core";
+import ContactCard from "./ContactCard.vue";
+import { get, map } from "lodash";
 
-export default Vue.extend({
-  name: "LandingPage",
+const url = ref("https://randomuser.me/api/?results=100");
+const refetch = ref(false);
+const items = ref([]);
+const { data, isFetching } = useFetch(url, { refetch }).get();
 
-  data: () => ({}),
+const contacts = reactive({
+  isFetching,
+
+  data: computed(() => {
+    try {
+      const tempData = JSON.parse(data.value as string);
+      if (tempData.results) {
+        return map(tempData.results, (contact) => {
+          const name = `${get(contact, "name.title", null)}.${get(
+            contact,
+            "name.first",
+            null
+          )} ${get(contact, "name.last", null)}`;
+          return {
+            name: name,
+            src: get(contact, "picture.large", null),
+          };
+        });
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return null;
+    }
+  }),
 });
 </script>
